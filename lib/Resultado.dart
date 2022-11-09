cb1812e2144f8b8f511be339ae22a511a122cc28
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-11-09 00:57:17 -0300

    Finalizada interface da tela Resultado

---
+++ lib/Resultado.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatefulWidget {
+  const Resultado({Key? key}) : super(key: key);
+
+  @override
+  State<Resultado> createState() => _ResultadoState();
+}
+
+class _ResultadoState extends State<Resultado> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff61bd86),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset("images/moeda_cara.png"),
+            GestureDetector(
+              onTap: (){},
+              child: Image.asset("images/botao_voltar.png"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
