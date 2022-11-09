6b3f679ee40d583d0ec1883a10fde4ccd80d4384
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-11-09 00:56:05 -0300

    Implementado método exibirResultado

--- lib/Jogar.dart
+++ lib/Jogar.dart
@@ -1,3 +1,4 @@
+import 'package:cara_coroa/Resultado.dart';
 import 'package:flutter/material.dart';
 
 class Jogar extends StatefulWidget {
@@ -10,7 +11,12 @@ class Jogar extends StatefulWidget {
 class _JogarState extends State<Jogar> {
 
   void _exibirResultado() {
-
+    Navigator.push(
+        context, 
+        MaterialPageRoute(
+            builder: (context) => Resultado()
+        )
+    );
   }
 
   @override
