f49a88eaaee75e497544e0db8dd6b6a1c0effb2b
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-11-09 00:28:16 -0300

    Commit inicial

---
+++ android/app/src/main/kotlin/io/github/danielperesjr/cara_coroa/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.danielperesjr.cara_coroa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Jogar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Jogar extends StatefulWidget {
+  const Jogar({Key? key}) : super(key: key);
+
+  @override
+  State<Jogar> createState() => _JogarState();
+}
+
+class _JogarState extends State<Jogar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
